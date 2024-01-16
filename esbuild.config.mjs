3617d5dc3776f122d08c7d985274b16d8fd18c54
Conner Bachmann <bac@christmann.info>
2024-01-16 09:16:00 +0100

    chore: setup for first publish

--- esbuild.config.mjs
+++ esbuild.config.mjs
@@ -5,9 +5,26 @@ const configs = [
   {
     entryPoints: ["./src/index.ts"],
     bundle: true,
-    outfile: "./dist/index.js",
+    outfile: "./dist/index.cjs",
+    format: "cjs",
     minify: true,
     sourcemap: true,
   },
+  {
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    outfile: "./dist/index.mjs",
+    format: "esm",
+    minify: true,
+    sourcemap: true,
+  },
+  {
+    entryPoints: ["./src/index.ts"],
+    bundle: true,
+    outfile: "./dist/index.js",
+    format: "iife",
+    minify: true,
+    sourcemap: true,
+  }
 ];
 export default configs;
